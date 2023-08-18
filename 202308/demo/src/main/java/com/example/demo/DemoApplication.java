7540f9e2abb1554779bdbf522571dbb27f1c690a
recordhyo <gytla1@naver.com>
2023-08-18 16:06:19 +0900

    springboot coffee 230818

--- 202308/demo/src/main/java/com/example/demo/DemoApplication.java
+++ 202308/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,21 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +24,102 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+@Entity
+
+class Coffee {
+	@Id
+	private String id;
+	private String name;
+
+	public Coffee(){
+
+	}
+
+	//생성자 2개 생성
+	//매개변수 모두 사용하는 생성자
+	public Coffee(String id, String name){
+		this.id = id;
+		this.name = name;
+	}
+
+	//이름만 사용하는 생성자 - id값 기본으로 제공함
+	public Coffee(String name){
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setID(String id){
+		this.id = id;
+	}
+
+}
+
+@RestController
+@RequestMapping("/coffees")
+class RestApiDemoController {
+	private final CoffeeRepository coffeeRepository;
+
+	public RestApiDemoController(CoffeeRepository coffeeRepository){
+		this.coffeeRepository = coffeeRepository;
+	}
+
+	@GetMapping
+	Iterable<Coffee> getCoffees(){
+		return coffeeRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	Optional<Coffee> getCoffeeById(@PathVariable String id){
+		return coffeeRepository.findById(id);
+	}
+
+	@PostMapping
+	Coffee postCoffee(@RequestBody Coffee coffee){
+		return coffeeRepository.save(coffee);
+	}
+
+	@PutMapping("/{id}")
+	ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee){
+		int coffeeIndex = -1;
+
+		return (coffeeRepository.existsById(id)) ?
+				new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK) :
+				new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
+	}
+
+	@DeleteMapping("/{id}")
+	void deleteCoffee(@PathVariable String id){
+		coffeeRepository.deleteById(id);
+	}
+
+
+}
+
+@Component
+class DataLoader{
+	private final CoffeeRepository coffeeRepository;
+	public DataLoader(CoffeeRepository coffeeRepository){
+		this.coffeeRepository = coffeeRepository;
+	}
+	@PostConstruct
+	private void loadData(){
+		coffeeRepository.saveAll(List.of(
+				new Coffee("Americano"),
+				new Coffee("latte"),
+				new Coffee("coldbrew")
+		));
+	}
 }
