9a16c70cd779fa9ab0c6e109a3055c1a61c57ab6
recordhyo <gytla1@naver.com>
2023-08-23 17:45:49 +0900

    blog post get

---
+++ 202308/blog/src/main/java/com/example/blog/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,28 @@
+package com.example.blog.springbootdeveloper.service;
+
+import com.example.blog.springbootdeveloper.domain.Article;
+import com.example.blog.springbootdeveloper.dto.AddArticleRequest;
+import com.example.blog.springbootdeveloper.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found:" +id));
+    }
+}
